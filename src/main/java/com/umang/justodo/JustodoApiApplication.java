f184fe57e03e8333f3cc9b1330596f380e211a2c
Umang Patel <upatel@ibaset.com>
2020-01-02 14:46:43 +0530

    Rename do to justodo.

--- src/main/java/com/umang/justodo/JustodoApiApplication.java
+++ src/main/java/com/umang/justodo/JustodoApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.umang.justodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoApiApplication {
+public class JustodoApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoApiApplication.class, args);
+		SpringApplication.run(JustodoApiApplication.class, args);
 	}
 
 }
